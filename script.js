b3ed89217eaf1b06675511fb2a315b66758679ba
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-02 01:52:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', function(){
+	let name= document.getElementById('name').value;
+    let age=document.getElementById('age').value;
+
+	let proms = new Promise((resolve , reject)=>{
+	if(age>18){
+		resolve({data:true});
+	}
+	else{
+		reject({data:false});
+	}
+});
+
+	proms.then((data)=>{
+	alert(`Welcome, ${name}. You can vote.`);
+}).catch((error)=>{
+	alert(`Oh sorry ${name}. You aren't old enough.`);
+});
+})
+
