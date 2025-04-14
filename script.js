d09931dfa309d08aeb525b4f740056f038632496
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-14 22:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ document.getElementById('btn').addEventListener('click',function(){
 	let age=pareseInt(document.getElementById("age").value);
 	let name=document.getElementById("name").value;
 	
+	// let prom = new Promise((resolve, reject)=>{
+	// 	setTimeout(()=>{
+	// 		if(age>18) resolve('y');
+	// 		else if(age<18) resolve('n');
+	// 	},4000)
+	// })
+
 	let prom  = new Promise((resolve, reject)=>{
 		setTimeout(()=>{
 			if(age>18){
-				resolve({data:'y'});
+				resolve('y');
 			}
 			else{
-				reject({data:'n'})
+				reject('n')
 			}
 		},4000);
 	})
+
 	prom.then((data)=> data=='y' ? alert(`Welcome, ${name}. You can vote.`) : alert(`Oh sorry ${name}. You aren't old enough.`))
 	
 })
